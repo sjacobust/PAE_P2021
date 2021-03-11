8800ad9632557a6bc53c44de9c68a4198b1d11d2
OverripeWings3 <sjacobust@gmail.com>
2021-03-11 11:00:49 -0600

    Adding Dotenv and MongoDB

--- Practicas/index.js
+++ Practicas/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const handlebars = require('express-handlebars');
-const port = process.env.PORT || 3000;
-const { NewsModel } = require('./src/models');
-const { newsRouter, usersRouter} = require('./routes');
+const dotenv = require('dotenv');
 
+const { newsRouter, usersRouter} = require('./routes');
+const MongoClient = require('mongodb').MongoClient;
+const { NewsModel } = require('./src/models');
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL;
 const newsModel = new NewsModel();
 
 // Static Files
@@ -20,10 +26,21 @@ app.use('/news', newsRouter);
 app.use('/users', usersRouter);
 
 app.get('/', (req, res) => {
-    newsModel.getAll(req, res)
+    newsModel.getAll(req, res);
 });
 
 
 app.listen(port, () => {
     console.log(`App is running in: http://localhost:${port}`);
+});
+
+
+MongoClient.connect(mongoURL, { useUnifiedTopology: true }, (err, client) => {
+    if(err) {
+        console.log('Failed to connect to MongoDB');
+        return;
+    }
+
+    const db = client.db();
+    console.log('Successfully connected to MongoDB');
 });
